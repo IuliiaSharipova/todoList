6adbe5086a1f3b951458ff9fe8b2f95155a7e27f
Iuliia <sharipova.y.s@gmail.com>
2022-11-23 15:23:07 +0100

    modified jest test

--- integration/input.test.js
+++ integration/input.test.js
@@ -1,11 +1,10 @@
 describe('Input', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
-
-        await page.goto('http://localhost:9009/iframe.html?args=&id=todolist-components-input--input-story&viewMode=story')
-        const image = await page.screenshot()
+        await page.goto('http://localhost:9009/iframe.html?args=&id=todolist-components-input--input-story&viewMode=story');
+        const image = await page.screenshot();
 
         // API from jest-image-snapshot
-        expect(image).toMatchImageSnapshot()
-    })
-})
+        expect(image).toMatchImageSnapshot();
+    });
+});
